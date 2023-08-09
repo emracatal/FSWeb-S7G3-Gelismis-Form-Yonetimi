32ae1d9c0e05a704881c079c64969cfb98f5f339
emracatal <134175723+emracatal@users.noreply.github.com>
2023-08-09 20:00:45 +0300

    form oluşturuldu

--- kullanici-katilimi/src/components/Form.js
+++ kullanici-katilimi/src/components/Form.js
@@ -1,5 +1,35 @@
+import React from "react";
 const Form = () => {
-  return <div>form.js'den bildiriyorum</div>;
+  return (
+    <form onSubmit={() => {}}>
+      <label htmlFor="name">
+        Name
+        <input id="name" type="text" placeholder="Name&Surname" />
+      </label>
+      <br />
+      <label htmlFor="user-mail">
+        Email
+        <input id="user-mail" type="email" placeholder="Email adress" />
+      </label>
+      <br />
+      <label htmlFor="user-pass">
+        Password
+        <input id="user-pass" type="password" placeholder="Password" />
+      </label>
+      <br />
+      <label htmlFor="checkbox">
+        Terms
+        <input type="checkbox" id="checkbox" />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
 };
 
 export default Form;
+
+/* - [ ] İsim (ismi, soyismi)
+- [ ] Email
+- [ ] Şifre
+- [ ] Kullanım Şartları (Terms of Service) (checkbox)
+- [ ] Gönder butonu (formu göndermek için). */
